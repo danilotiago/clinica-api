f59d4aa65bc8b3a7e7b5fbaa07f6cd82bb3b209c
Gian99Santos <gian.santos@aluno.faculdadeimpacta.com.br>
2020-01-23 23:22:37 -0300

    criando novos estados

--- app/seeders/states.seeder.ts
+++ app/seeders/states.seeder.ts
@@ -15,13 +15,138 @@ class StateSeeder implements ISeeder {
     createStates(): void {
 
         statesRepository.save({
-            name: 'São Paulo',
-            abbreviation: 'SP'
+            name: 'Acre',
+            abbreviation: 'AC'
         })
-        
+
+        statesRepository.save({
+            name: 'Alagoas',
+            abbreviation: 'AL'
+        })
+
+        statesRepository.save({
+            name: 'Amapá',
+            abbreviation: 'AP'
+        })
+
+        statesRepository.save({
+            name: 'Amazonas',
+            abbreviation: 'AM'
+        })
+
+        statesRepository.save({
+            name: 'Bahia',
+            abbreviation: 'BA'
+        })
+
+        statesRepository.save({
+            name: 'Ceará',
+            abbreviation: 'CE'
+        })
+
+        statesRepository.save({
+            name: 'Distrito Federal',
+            abbreviation: 'DF'
+        })
+
+        statesRepository.save({
+            name: 'Espírito Santo',
+            abbreviation: 'ES'
+        })
+
+        statesRepository.save({
+            name: 'Goiás',
+            abbreviation: 'GO'
+        })
+
+        statesRepository.save({
+            name: 'Maranhão',
+            abbreviation: 'MA'
+        })
+
+        statesRepository.save({
+            name: 'Mato Grosso',
+            abbreviation: 'MT'
+        })
+
+        statesRepository.save({
+            name: 'Mato Grosso do Sul',
+            abbreviation: 'MS'
+        })
+
+        statesRepository.save({
+            name: 'Minas Gerais',
+            abbreviation: 'MG'
+        })
+
+        statesRepository.save({
+            name: 'Pará',
+            abbreviation: 'PA'
+        })
+
+        statesRepository.save({
+            name: 'Paraíba',
+            abbreviation: 'PB'
+        })
+
         statesRepository.save({
             name: 'Paraná',
             abbreviation: 'PR'
+        })
+
+        statesRepository.save({
+            name: 'Pernambuco',
+            abbreviation: 'PE'
+        })
+
+        statesRepository.save({
+            name: ' Piauí',
+            abbreviation: 'PI'
+        })
+
+        statesRepository.save({
+            name: 'Rio de Janeiro',
+            abbreviation: 'RJ'
+        })
+
+        statesRepository.save({
+            name: 'Rio Grande do Norte',
+            abbreviation: 'RN'
+        })
+
+        statesRepository.save({
+            name: 'Rio Grande do Sul',
+            abbreviation: 'RS'
+        })
+
+        statesRepository.save({
+            name: 'Rondônia',
+            abbreviation: 'RO'
+        })
+
+        statesRepository.save({
+            name: 'Roraima',
+            abbreviation: 'RR'
+        })
+
+        statesRepository.save({
+            name: 'Santa Catarina',
+            abbreviation: 'SC'
+        })
+
+        statesRepository.save({
+            name: 'São Paulo',
+            abbreviation: 'SP'
+        })
+
+        statesRepository.save({
+            name: 'Sergipe',
+            abbreviation: 'SE'
+        })
+
+        statesRepository.save({
+            name: 'Tocantis',
+            abbreviation: 'TO'
         })
     }
 }
--- dist/app/seeders/states.seeder.js
+++ dist/app/seeders/states.seeder.js
@@ -10,141 +10,114 @@ class StateSeeder {
         });
     }
     createStates() {
-
-        statesRepository.save({
+        states_repository_1.statesRepository.save({
             name: 'Acre',
             abbreviation: 'AC'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Alagoas',
             abbreviation: 'AL'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Amapá',
             abbreviation: 'AP'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Amazonas',
             abbreviation: 'AM'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Bahia',
             abbreviation: 'BA'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Ceará',
             abbreviation: 'CE'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Distrito Federal',
             abbreviation: 'DF'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Espírito Santo',
             abbreviation: 'ES'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Goiás',
             abbreviation: 'GO'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Maranhão',
             abbreviation: 'MA'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Mato Grosso',
             abbreviation: 'MT'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Mato Grosso do Sul',
             abbreviation: 'MS'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Minas Gerais',
             abbreviation: 'MG'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Pará',
             abbreviation: 'PA'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Paraíba',
             abbreviation: 'PB'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Paraná',
             abbreviation: 'PR'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Pernambuco',
             abbreviation: 'PE'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: ' Piauí',
             abbreviation: 'PI'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Rio de Janeiro',
             abbreviation: 'RJ'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Rio Grande do Norte',
             abbreviation: 'RN'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Rio Grande do Sul',
             abbreviation: 'RS'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Rondônia',
             abbreviation: 'RO'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Roraima',
             abbreviation: 'RR'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Santa Catarina',
             abbreviation: 'SC'
-        })
-
+        });
         states_repository_1.statesRepository.save({
             name: 'São Paulo',
             abbreviation: 'SP'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Sergipe',
             abbreviation: 'SE'
-        })
-
-        statesRepository.save({
+        });
+        states_repository_1.statesRepository.save({
             name: 'Tocantis',
             abbreviation: 'TO'
-        })
+        });
     }
 }
 exports.stateSeeder = new StateSeeder();
