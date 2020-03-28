0bc57e616e28e56080ab838afd2a71228a9cc731
Default Profile <Default Profile>
2020-03-28 15:10:07 -0300

    correcao config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,17 +4,10 @@ module.exports = {
       script : "./dist/main.js",
       instances : 0,
       exec_mode : "cluster",
-      merge_logs: true,
-      //watch: true,
       env : {
         SERVER_PORT : 3000,
         DB_URL  : 'mongodb://localhost/clinica',
         NODE_ENV : 'development'
-      },
-      env_production: {
-        SERVER_PORT : 3001,
-        DB_URL  : 'mongodb://localhost/clinica',
-        NODE_ENV : 'production'
       }
     }]
   }
