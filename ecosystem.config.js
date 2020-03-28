b5094d4182b1b57e3056ef5f926f635aaef5c54c
Default Profile <Default Profile>
2020-03-28 15:01:03 -0300

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps : [{
+      name   : "clinica-api",
+      script : "./dist/main.js",
+      instances : 0,
+      exec_mode : "cluster",
+      merge_logs: true,
+      //watch: true,
+      env : {
+        SERVER_PORT : 3000,
+        DB_URL  : 'mongodb://localhost/clinica',
+        NODE_ENV : 'development'
+      },
+      env_production: {
+        SERVER_PORT : 3001,
+        NODE_ENV : 'production'
+      }
+    }]
+  }
+  
